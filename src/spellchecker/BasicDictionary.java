9f61f70bdb49bf7d3f67d1cb21bc1fa0cd52e93e
thesakib <sakibkool@gmail.com>
2017-11-01 13:29:34 -0700

    Initial commit

---
+++ src/spellchecker/BasicDictionary.java
@@ -0,0 +1,47 @@
+package spellchecker;
+
+public class BasicDictionary implements Dictionary {
+
+	@Override
+	public void importFile(String filename) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void load(String filename) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save(String filename) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String[] find(String word) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(String word) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public BinaryTreeNode getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/spellchecker/BasicSpellChecker.java
@@ -0,0 +1,59 @@
+package spellchecker;
+
+public class BasicSpellChecker implements SpellChecker {
+
+	@Override
+	public void importDictionary(String filename) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadDictionary(String filename) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void saveDictionary(String filename) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadDocument(String filename) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void saveDocument(String filename) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] spellCheck(boolean continueFromPrevious) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addWordToDictionary(String word) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void replaceText(int startIndex, int endIndex, String replacementText) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
